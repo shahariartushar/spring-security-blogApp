1e39c132ee44453d8a43e63d72d8c653a33e87ce
shahariartushar <shahariartushar@gmail.com>
2023-03-06 20:58:21 +0100

    feat: add basic security

--- src/main/java/com/springsecurity/SpringSecurity/Mapper/PostMapper.java
+++ src/main/java/com/springsecurity/SpringSecurity/Mapper/PostMapper.java
@@ -2,10 +2,14 @@ package com.springsecurity.SpringSecurity.Mapper;
 
 import com.springsecurity.SpringSecurity.model.Post;
 import com.springsecurity.SpringSecurity.payload.PostDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 public class PostMapper {
+
 	public static Post mapToModel(PostDTO postDTO) {
+		//Post post = modelMapper.map(postDTO, Post.class);
+
 		Post post = new Post();
 		post.setTitle(postDTO.getTitle());
 		post.setDesc(postDTO.getDesc());
@@ -15,6 +19,8 @@ public class PostMapper {
 	}
 	
 	public static PostDTO mapToDTO(Post post) {
+		//PostDTO postResponseDto = modelMapper.map(post, PostDTO.class);
+
 		PostDTO postResponseDto = new PostDTO();
 		postResponseDto.setId(post.getId());
 		postResponseDto.setTitle(post.getTitle());
